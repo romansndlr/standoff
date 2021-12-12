bed5292d7b933d481d1ee4f27b1e15b548642563
Roman Sandler <roman.sandler@fiverr.com>
2021-12-12 12:48:56 +0200

    background image optimization

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -10,7 +10,9 @@ interface TableStatic {
 const Table: React.FC & TableStatic = ({ children }) => (
   <div
     className={`h-full bg-green-800 p-4 md:p-8 lg:p-12 xl:p-14 bg-cover bg-center bg-no-repeat`}
-    style={{ backgroundImage: `url('${tableBackgroundUrl}')` }}
+    style={{
+      backgroundImage: `url('https://res.cloudinary.com/romangutkin/image/upload/q_auto/v1639305835/table-background.jpg')`,
+    }}
   >
     <div className="flex flex-col space-y-4">{children}</div>
   </div>
